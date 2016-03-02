e2298bf26c7c307b60832eee1eaf2ad61d494953
unknown <steven@snorwood.com>
2016-03-02 01:12:27 -0500

    initial commit

---
+++ wally.cpp
@@ -0,0 +1,30 @@
+#include "wally.h"
+#include <Adafruit_MMA8451.h>
+#include <NewPing.h>
+
+float adcConvert(int count) {
+  return count * ADC_CONVERT;
+}
+
+mma = Adafruit_MMA8451();
+
+void calibrateAccelerometer(){
+	accelerometerOffset.x = 0;
+	accelerometerOffset.y = 0;
+	accelerometerOffset.z = 0;
+}
+
+XYZ readAccelerometer() {
+	XYZ accData;
+	
+	mma.read();
+	accData.x = adcConvert(mma.x);
+	accData.y = adcConvert(mma.y);
+	accData.z = adcConvert(mma.z); 
+
+	return accData;
+}
+
+float readUltrasonic(int address) {
+	return 1;
+}
---
+++ wally.h
@@ -0,0 +1,46 @@
+/*
+  wally.h - Library for controlling wally the wall climbing robot.
+  Created by Team Group 2, March 1, 2016.
+*/
+#ifndef wally_h
+#define wally_h
+
+// Defines Motor Pins
+const int LF_L = 2;
+const int LF_H = 3;
+const int RF_H = 4;
+const int RF_L = 5;
+const int LR_L = 6;
+const int LR_H = 7;
+const int RR_H = 8;
+const int RR_L = 9;
+
+
+
+
+const float ADC_CONVERT = 8 * 9.81 / 16384;
+
+// struct xyz -> Base of XYZ
+struct xyz {
+	float x;
+	float y;
+	float z;
+};
+
+/*
+	Struct: XYZ
+	Description: Stores three dimensional float data
+*/
+typedef struct xyz XYZ;
+
+Adafruit_MMA8451 mma;
+XYZ accelerometerOffset;
+
+float adcConvert(int count);
+
+float readUltrasonic(int address);
+
+void calibrateAccelerometer();
+XYZ readAccelerometer();
+
+#endif
