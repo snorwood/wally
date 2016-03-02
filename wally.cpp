ce3c1406bcf55368205b8f73c734b98a5ea24308
Steven Norwood <steven@snorwood.com>
2016-03-02 12:27:41 -0500

    Added wally class

--- wally.cpp
+++ wally.cpp
@@ -1,36 +1,35 @@
 #include "wally.h"
-#include <Adafruit_MMA8451.h>
-#include <NewPing.h>
 
-float adcConvert(int count) {
+Wally::Wally() {
+	mma = new Adafruit_MMA8451();
+	us_f = new NewPing(US_F_T, US_F_E, US_MAX_DISTANCE);
+	us_r = new NewPing(US_R_T, US_R_E, US_MAX_DISTANCE);
+}
+
+float Wally::convertAccelerometer(int count) {
   return count * ADC_CONVERT;
 }
 
-Adafruit_MMA8451 mma = Adafruit_MMA8451();
-XYZ accelerometerOffset;
-NewPing us_f = NewPing(US_F_T, US_F_E, US_MAX_DISTANCE);
-NewPing us_r = NewPing(US_R_T, US_R_E, US_MAX_DISTANCE);
-
-void calibrateAccelerometer(){
+void Wally::calibrateAccelerometer(){
 	accelerometerOffset.x = 0;
 	accelerometerOffset.y = 0;
 	accelerometerOffset.z = 0;
 }
 
-XYZ readAccelerometer() {
+XYZ Wally::readAccelerometer() {
 	XYZ accData;
 	
-	mma.read();
-	accData.x = adcConvert(mma.x);
-	accData.y = adcConvert(mma.y);
-	accData.z = adcConvert(mma.z); 
+	mma->read();
+	accData.x = convertAccelerometer(mma->x);
+	accData.y = convertAccelerometer(mma->y);
+	accData.z = convertAccelerometer(mma->z); 
 
 	return accData;
 }
 
-float readUltrasonic(int address) {
-	if address == 0
-		return us_f.ping() / float(US_CONVERT);
+float Wally::readUltrasonic(int address) {
+	if (address == 0)
+		return us_f->ping() / float(US_CONVERT);
 
-	return us_r.ping() / float(US_CONVERT);
+	return us_r->ping() / float(US_CONVERT);
 }
--- wally.h
+++ wally.h
@@ -5,7 +5,10 @@
 #ifndef wally_h
 #define wally_h
 
-// Defines Motor Pins
+#include <Adafruit_MMA8451.h>
+#include <NewPing.h>
+
+/* Defines Motor Constants */
 const int LF_L = 2;
 const int LF_H = 3;
 const int RF_H = 4;
@@ -15,7 +18,7 @@ const int LR_H = 7;
 const int RR_H = 8;
 const int RR_L = 9;
 
-// Define ultrasonic pins
+/* Define Ultrasonic Constants */
 const int US_F_E = 1;
 const int US_F_T = 2;
 const int US_R_E = 3;
@@ -23,8 +26,12 @@ const int US_R_T = 4;
 const int US_MAX_DISTANCE = 200;
 const int US_CONVERT = 58;
 
-// Define accelerometer constants
+/* Define Accelerometer Constants */
 const float ADC_CONVERT = 8 * 9.81 / 16384;
+
+/* Define Button Constants */
+const int BUTTON = 40;
+const int BUTTON_REF = 36;
 
 // struct xyz -> Base of XYZ
 struct xyz {
@@ -39,11 +46,23 @@ struct xyz {
 */
 typedef struct xyz XYZ;
 
-float adcConvert(int count);
+class Wally {
+public:
+	Wally();
 
-float readUltrasonic(int address);
+	/* Ultrasonic */
+	float readUltrasonic(int address);
 
-void calibrateAccelerometer();
-XYZ readAccelerometer();
+	/* Accelerometer */
+	void calibrateAccelerometer();
+	XYZ readAccelerometer();
+
+private:
+	Adafruit_MMA8451* mma;
+	XYZ accelerometerOffset;
+	float convertAccelerometer(int count);
+	NewPing* us_f;
+	NewPing* us_r;
+};
 
 #endif
